6097f80bcf6a8e10fe91bdae1314e8a984f0c519
luminousbeam <jocelyn.jeriah@gmail.com>
2019-11-26 14:50:49 -0500

    first java program assignment

---
+++ 01-java-fundamentals/02-fundamentals/01-first-java-program/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Jocelyn");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Washington, DC");
+    }
+}
