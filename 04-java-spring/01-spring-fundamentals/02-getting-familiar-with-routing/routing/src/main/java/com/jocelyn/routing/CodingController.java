cc7ae879821c0cf060cb6401899bc8a9e2ad5cc1
luminousbeam <jocelyn.jeriah@gmail.com>
2019-12-07 17:41:20 -0500

    cleaned up Coding Controller

--- 04-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/routing/src/main/java/com/jocelyn/routing/CodingController.java
+++ 04-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/routing/src/main/java/com/jocelyn/routing/CodingController.java
@@ -30,20 +30,3 @@ public class CodingController {
 		return "Java/Spring is better!";
 	}
 }
-
-
-//	public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
-//	    return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
-//	}
-
-
-/*
-Create a controller named 'CodingController'. For the first 3 routes, use the @RequestMapping annotation.
-Have an http GET request to 'http://localhost:8080/coding' display a text that says 'Hello Coding Dojo!'.
-Have an http GET request to 'http://localhost:8080/coding/python' display a text that says 'Python/Django was awesome!'.
-Have an http GET request to 'http://localhost:8080/coding/java' display a text that says 'Java/Spring is better!'.
-Create another controller named 'DojoController'. For the routes below, use the @PathVariable annotation.
-Have an http GET request to 'http://localhost:8080/dojo' display a text that says 'The dojo is awesome!'.
-Have an http GET request to 'http://localhost:8080/burbank-dojo/' display a text that says 'Burbank Dojo is located in Southern California'.
-Have an http GET request to 'http://localhost:8080/san-jose/' display a text that says 'SJ dojo is the headquarters'.
-*/
