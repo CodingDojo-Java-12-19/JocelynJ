44bcac077aa9c54d77b1094df24c998979a8f6b8
luminousbeam <jocelyn.jeriah@gmail.com>
2019-12-09 17:03:44 -0500

    completed Update and Delete assignment

---
+++ 04-java-spring/02-spring-data-i/01-update-and-delete/mvc/src/test/java/com/jocelyn/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jocelyn.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
